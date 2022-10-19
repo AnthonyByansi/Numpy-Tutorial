14f6ca699e6bc512feaf50db094d82b3261ebc84
Anthony_256 <101401469+AnthonyByansi@users.noreply.github.com>
2022-10-19 13:01:14 +0300

    sorting arrays

---
+++ Introduction/Arrays/sorting_array.py
@@ -0,0 +1,11 @@
+"""
+with sorting, we put elements in ordered sequence 
+here we make use of the sort() function
+"""
+
+import numpy as np
+
+arr = np.array([[1,3,4,5,2,6], [2,4,5,62,5,7]])
+arr1 = np.array(['Owen', 'Lynnate', 'Bill'])
+print(np.sort(arr))
+print(np.sort(arr1))
